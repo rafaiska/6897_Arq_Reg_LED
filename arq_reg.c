67c0e4662ff8618fe8d460e2357f323bb2145d36
Rafaiska <paocomcarnemoidaetubaina@gmail.com>
2016-06-06 17:24:28 -0300

    insercao no fim do arquivo de registro funcionando

--- arq_reg.c
+++ arq_reg.c
@@ -1,5 +1,63 @@
 #include "arq_reg.h"
 
+uint16_t Calcular_Tamanho(registro_t registro)
+{
+	uint16_t retorno = 0;
+
+	retorno += 5; //5 bytes para os separadores '|'
+	retorno += sizeof(uint32_t); //4 bytes para registro_t.id (uint32_t)
+	retorno += strlen(registro.autor);
+	retorno += strlen(registro.titulo);
+	retorno += strlen(registro.curso);
+	retorno += strlen(registro.tipo);
+
+	return retorno;
+}
+
+int Inserir_Registro(char *caminho_registro, registro_t registro)
+{
+	FILE *arq_reg;
+	uint16_t first_led;
+	int retorno;
+
+	arq_reg = Abrir_arquivo(caminho_registro, "a+");
+	fseek(arq_reg, 0, SEEK_SET);
+	fread(&first_led, sizeof(uint16_t), 1, arq_reg);
+
+	if(first_led == 0)
+		retorno = Inserir_Registro_Final(arq_reg, registro);
+	else
+	{
+		//TODO: Percorrer a LED e inserir no primeiro espaco da lista. Atualizar a lista depois
+	}
+
+	fclose(arq_reg);
+	return retorno;
+}
+
+int Inserir_Registro_Final(FILE *arq_reg, registro_t registro)
+{
+	uint16_t tamanho;
+	int retorno;
+
+	tamanho = Calcular_Tamanho(registro);
+	fseek(arq_reg, 0, SEEK_END);
+	retorno = ftell(arq_reg);
+	fwrite(&tamanho, sizeof(uint16_t), 1, arq_reg);
+	fwrite(&(registro.id), sizeof(uint32_t), 1, arq_reg);
+	fputc('|', arq_reg);
+	fwrite(registro.autor, sizeof(char), strlen(registro.autor), arq_reg);
+	fputc('|', arq_reg);
+	fwrite(registro.titulo, sizeof(char), strlen(registro.titulo), arq_reg);
+	fputc('|', arq_reg);
+	fwrite(registro.curso, sizeof(char), strlen(registro.curso), arq_reg);
+	fputc('|', arq_reg);
+	fwrite(registro.tipo, sizeof(char), strlen(registro.tipo), arq_reg);
+	fputc('|', arq_reg);
+
+	return retorno;
+}
+
 registro_t String_to_reg(char *cadeia)
 {
 	registro_t retorno;
@@ -15,13 +73,13 @@ registro_t String_to_reg(char *cadeia)
 	campo = strtok(cadeia, ";");
 	retorno.id = (uint32_t) (atoi(campo));
 	campo = strtok(NULL, ";");
-	strcpy(retorno.autor, campo);
+	strcpy(retorno.autor, campo+1);
 	campo = strtok(NULL, ";");
-	strcpy(retorno.titulo, campo);
+	strcpy(retorno.titulo, campo+1);
 	campo = strtok(NULL, ";");
-	strcpy(retorno.curso, campo);
-	campo = strtok(NULL, ";");
-	strcpy(retorno.tipo, campo);
+	strcpy(retorno.curso, campo+1);
+	campo = strtok(NULL, "\r");
+	strcpy(retorno.tipo, campo+1);
 
 	return retorno;
 }
@@ -29,10 +87,35 @@ registro_t String_to_reg(char *cadeia)
 FILE *Abrir_arquivo(char *caminho, char *modo)
 {
 	FILE *arquivo;
+	uint16_t first_led = 0;
 
-	arquivo = fopen(caminho,modo);
-	if(arquivo == NULL)
-        	perror("Falha ao carregar arquivo: ");
+	if(modo[0] == 'r')
+	{
+		arquivo = fopen(caminho,modo);
+		if(arquivo == NULL)
+		{
+			printf("Falha ao carregar arquivo em '%s'\n", caminho);
+        		perror("Mensagem do sistema");
+			exit(1);
+		}
+	}
+	else
+	{
+		arquivo = fopen(caminho,"r");
+
+		//Arquivo de registros com LED nao existe. Cria um arquivo vazio
+		if(arquivo == NULL)	
+		{
+			arquivo = fopen(caminho, modo);
+			fwrite(&first_led, sizeof(uint16_t), 1, arquivo);
+		}
+		//Abre o arquivo existente em modo de escrita ou append
+		else
+		{
+			fclose(arquivo);
+			arquivo = fopen(caminho, modo);
+		}
+	}
 
 	return arquivo;
 }
@@ -42,6 +125,7 @@ int Importar_arquivo_catalogo(char *caminho_catalogo, char *caminho_registro)
 	char leitura[1024];
 	char c;
 	int i;
+	int n_reg = 0;
 	registro_t lido;
 	FILE *catalogo;
 
@@ -61,8 +145,12 @@ int Importar_arquivo_catalogo(char *caminho_catalogo, char *caminho_registro)
 			break;	//acabou o arquivo
 
 		lido = String_to_reg(leitura);
-		printf("%x, %s, %s, %s, %s\n\n", lido.id, lido.autor, lido.titulo, lido.curso, lido.tipo);
 
 		//TODO: ESCREVER REZISTRO NO ARQUIVO
-	}		
+		i = Inserir_Registro(caminho_registro, lido);
+		printf("Inserido registro na posicao %d do arquivo.\n", i);
+		++n_reg;
+	}
+
+	return n_reg;
 }
--- main.c
+++ main.c
@@ -1,9 +1,70 @@
 #include "arq_reg.h"
 
+uint8_t ECHO = 0;
+char CAMINHO_REGISTRO[256] = "./res/rezistro.rez\0";
+
+void Print_Menu()
+{
+	printf("Escolha uma opcao:\n");
+	printf("\t1. Importar arquivo de catalogo\n");
+	printf("\t2. Adicionar registro ao arquivo\n");
+	printf("\t3. Apagar registro do arquivo\n");
+	printf("\t4. Buscar registro no arquivo\n");
+	printf("\t5. Sair\n");
+}
+
+void Opcao1()
+{
+	char CAMINHO_CATALOGO[256];
+	char c;
+	int i = 0;
+
+	getchar(); //LIMPA O BUFFER
+	printf("Qual o caminho para o arquivo de catalogo? (Caso deixe em branco, sera usado o caminho padrao: './res/catalogo.txt')\n");
+	while((c = getchar()) != '\n')
+	{
+		CAMINHO_CATALOGO[i] = c;
+		++i;
+	}
+	CAMINHO_CATALOGO[i] = '\0';
+
+	if(strlen(CAMINHO_CATALOGO) == 0)
+		Importar_arquivo_catalogo("./res/catalogo.txt", CAMINHO_REGISTRO);
+	else
+		Importar_arquivo_catalogo(CAMINHO_CATALOGO, CAMINHO_REGISTRO);
+}
 
 int main(int argc, char *argv[])
 {
-	Importar_arquivo_catalogo("./res/catalogo.txt", NULL);
+	int opcao=0;
+	int i;
+
+	for(i=0; i<argc; ++i)
+	{
+		if(!strcmp(argv[i],"--echo"))
+			ECHO = 1;
+		else if(!strcmp(argv[i],"-r") && argc > i+1)
+			strcpy(CAMINHO_REGISTRO, argv[i+1]);
+	}
+
+	printf("Programa editor de arquivos binarios de registros com lista de espacos disponiveis (LED)\n");
+	printf("Arquivo de registro selecionado para edicao: %s\n\n", CAMINHO_REGISTRO);
+
+	while(opcao != 5)
+	{
+		Print_Menu();
+		scanf("%d", &opcao);
+		
+		switch(opcao)
+		{
+			case 1: Opcao1(); break;
+			case 2: break;
+			case 3: break;
+			case 4: break;
+			case 5: break;
+			default: printf("Opcao invalida!\n"); break;
+		}
+	}
 
 	return 0;
 }
