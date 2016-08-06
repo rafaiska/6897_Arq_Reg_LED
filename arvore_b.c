439b52c25f534182dcdb1bfc3ac9829f778d5907
Rafaiska <paocomcarnemoidaetubaina@gmail.com>
2016-08-06 00:30:09 -0300

    insercoes funcionando na arvore B

--- arvore_b.c
+++ arvore_b.c
@@ -1,8 +1,21 @@
 #include "arvore_b.h"
 
+uint16_t Carregar_Pagina(FILE *arq_arv, no_arvore_b_t *pagina)
+{
+	uint16_t RRN;
+	int i;
+	
+	RRN = POS_to_RRN((uint16_t)(ftell(arq_arv)));
+
+	fread(pagina, sizeof(no_arvore_b_t), 1, arq_arv);
+
+	return RRN;
+}
+
 uint16_t Criar_Pagina(FILE *arq_arv, no_arvore_b_t nova)
 {
 	uint16_t topo_pilha;
+	uint16_t pos_insert;
 	uint16_t RRN;
 	int i;
 
@@ -12,24 +25,31 @@ uint16_t Criar_Pagina(FILE *arq_arv, no_arvore_b_t nova)
 	if(topo_pilha == 0) //PED estah vazia
 	{
 		fseek(arq_arv, 0, SEEK_END);
-		RRN = ftell(arq_arv);
-		RRN -= 4;
-		RRN /= (ORDEM_ARVORE *2 + (ORDEM_ARVORE -1) *6);
-		RRN += 1;
-
-		fwrite(&(nova.n), sizeof(uint8_t), 1, arq_arv);
-		for(i=0; i< ORDEM_ARVORE; ++i)
-			fwrite(nova.filhas +i, sizeof(uint16_t), 1, arq_arv);
-		for(i=0; i< ORDEM_ARVORE -1; ++i)
-		{
-			fwrite(nova.id +i, sizeof(uint32_t), 1, arq_arv);
-			fwrite(nova.offset +i, sizeof(uint16_t), 1, arq_arv);	
-		}	
+		RRN = POS_to_RRN((uint16_t)(ftell(arq_arv)));
 	}
 	else
 	{
-		//TODO: Percorrer a PED
+		RRN = topo_pilha;
+		pos_insert = RRN_to_POS(topo_pilha);
+		fseek(arq_arv, pos_insert +1, SEEK_SET); //soma 1 para contabilizar o caractere '*'
+		fread(&topo_pilha, sizeof(uint16_t), 1, arq_arv);
+		fseek(arq_arv, 0, SEEK_SET);
+		fwrite(&topo_pilha, sizeof(uint16_t), 1, arq_arv);
+		fseek(arq_arv, pos_insert, SEEK_SET);
 	}
+
+	RRN = Escrever_Pagina(arq_arv, nova);
+
+	return RRN;
+}
+
+uint16_t Escrever_Pagina(FILE *arq_arv, no_arvore_b_t pagina)
+{
+	uint16_t RRN;
+	
+	RRN = POS_to_RRN((uint16_t)(ftell(arq_arv)));
+
+	fwrite(&pagina, sizeof(no_arvore_b_t), 1, arq_arv);
 
 	return RRN;
 }
@@ -37,9 +57,15 @@ uint16_t Criar_Pagina(FILE *arq_arv, no_arvore_b_t nova)
 uint16_t Inserir_No_ArvoreB(char *arquivo, uint32_t id, uint16_t offset)
 {
 	uint16_t raiz;
+	uint16_t raiz_pos;
 	uint16_t nova_rrn;
+	uint16_t promo_r_child;
+	uint32_t promo_key_id;
+	uint16_t promo_key_offset;
+	uint8_t flag;
 	FILE *arq_arv;
 	no_arvore_b_t nova;
+	no_arvore_b_t raiz_a;
 
 	arq_arv = fopen(arquivo, "r+");
 	fseek(arq_arv, 2, SEEK_SET);
@@ -58,39 +84,253 @@ uint16_t Inserir_No_ArvoreB(char *arquivo, uint32_t id, uint16_t offset)
 	}
 	else
 	{
-		//TODO: Inserir na arvore B
+		flag = Recursive_Insertion(arq_arv, raiz, id, offset, &promo_r_child, &promo_key_id, &promo_key_offset);
+		switch(flag)
+		{
+			case INSERTION_PROMOTION:
+					raiz_a.filhas[0] = raiz;
+					raiz_a.id[0] = promo_key_id;
+					raiz_a.offset[0] = promo_key_offset;
+					raiz_a.filhas[1] = promo_r_child;
+					raiz = Criar_Pagina(arq_arv, raiz_a);
+					fseek(arq_arv, 2, SEEK_SET);
+					fwrite(&raiz, sizeof(uint16_t), 1, arq_arv);
+				break;
+			case INSERTION_NO_PROMOTION: break;
+			case INSERTION_ERROR: break;
+		}
 	}
 
 	fclose(arq_arv);
 	return nova_rrn;
 }
 
-FILE *Inicializar_ArvoreB(char *arquivo)
+void Insertion_Split(FILE *arq_arv, no_arvore_b_t *pagina, int pos_insert, uint16_t *promo_r_child, uint32_t *promo_key_id, uint16_t *promo_key_offset)
+{
+	no_arvore_b_t nova_pagina;
+	int metade, i;
+
+	nova_pagina.n = 0;
+	metade = ORDEM_ARVORE /2;
+
+	if(pos_insert == metade)
+	{
+		for(i= metade; i< ORDEM_ARVORE -1; ++i)
+		{
+			nova_pagina.id[i -metade] = pagina->id[i];
+			nova_pagina.offset[i -metade] = pagina->offset[i];
+			nova_pagina.filhas[i -metade +1] = pagina->filhas[i+1];
+			--(pagina->n);
+			++(nova_pagina.n);
+		}
+		nova_pagina.filhas[0] = *promo_r_child;
+	}
+	else if(pos_insert < metade)
+	{
+		for(i=metade; i< ORDEM_ARVORE -1; ++i)
+		{
+			nova_pagina.id[i -metade] = pagina->id[i];
+			nova_pagina.offset[i -metade] = pagina->offset[i];
+			nova_pagina.filhas[i -metade +1] = pagina->filhas[i+1];
+			--(pagina->n);
+			++(nova_pagina.n);
+		}
+
+		for(i=ORDEM_ARVORE -2; i> pos_insert; --i)
+		{
+			pagina->id[i] = pagina->id[i-1];
+			pagina->offset[i] = pagina->offset[i-1];
+			pagina->filhas[i+1] = pagina->filhas[i];
+		}
+		pagina->id[pos_insert] = *promo_key_id;
+		pagina->offset[pos_insert] = *promo_key_offset;
+		pagina->filhas[pos_insert +1] = *promo_r_child;
+		*promo_key_id = pagina->id[metade];
+		*promo_key_offset = pagina->offset[metade];
+		nova_pagina.filhas[0] = pagina->filhas[metade +1];
+	}
+	else
+	{
+		for(i=metade +1; i< ORDEM_ARVORE -1; ++i)
+		{
+			nova_pagina.id[i -metade -1] = pagina->id[i];
+			nova_pagina.offset[i -metade -1] = pagina->offset[i];
+			nova_pagina.filhas[i -metade] = pagina->filhas[i+1];
+			--(pagina->n);
+			++(nova_pagina.n);
+		}
+
+		pos_insert -= (metade +1);
+		for(i=ORDEM_ARVORE -2; i> pos_insert; --i)
+		{
+			nova_pagina.id[i] = nova_pagina.id[i-1];
+			nova_pagina.offset[i] = nova_pagina.offset[i-1];
+			nova_pagina.filhas[i+1] = nova_pagina.filhas[i];
+		}
+		nova_pagina.id[pos_insert] = *promo_key_id;
+		nova_pagina.offset[pos_insert] = *promo_key_offset;
+		nova_pagina.filhas[pos_insert +1] = *promo_r_child;
+		*promo_key_id = pagina->id[metade];
+		*promo_key_offset = pagina->offset[metade];
+		nova_pagina.filhas[0] = pagina->filhas[metade +1];
+		--(pagina->n);
+		++(nova_pagina.n);
+	}
+
+	*promo_r_child = Criar_Pagina(arq_arv, nova_pagina);
+}
+
+uint8_t Inicializar_ArvoreB(char *arquivo)
 {
 	FILE *nova;
 	uint16_t ped = 0;
 	uint16_t raiz = 0;
 
-	nova = fopen(arquivo, "w");
-	fwrite(&ped, sizeof(uint16_t), 1, nova);
-	fwrite(&raiz, sizeof(uint16_t), 1, nova);
-	fclose(nova);
-	nova = fopen(arquivo, "r+");
+	nova = fopen(arquivo, "r");
+	if(nova == NULL)
+	{
+		nova = fopen(arquivo, "w");
+		if(nova == NULL)
+			return INICIALIZA_ARVORE_ERRO;
 
-	return nova;
+		fwrite(&ped, sizeof(uint16_t), 1, nova);
+		fwrite(&raiz, sizeof(uint16_t), 1, nova);
+		fclose(nova);
+		return INICIALIZA_ARVORE_JAEXISTE;
+	}
+
+	fclose(nova);
+	return INICIALIZA_ARVORE_SUCESSO;
+}
+
+uint16_t POS_to_RRN(uint16_t POS)
+{	
+	POS -= 4; //4 bytes do cabecalho
+	POS /= sizeof(no_arvore_b_t); //Divide pelo tamanho dos registros
+	POS += 1; //Os RRN das paginas comecam em 1
+	return POS;
+}
+
+uint16_t RRN_to_POS(uint16_t RRN)
+{	
+	--RRN; //Os RRN das paginas comecam em 1
+	RRN *= sizeof(no_arvore_b_t); //Multiplica pelo tamanho dos registros
+	RRN += 4; //4 bytes do cabecalho
+	return RRN;
+}
+
+uint8_t Recursive_Insertion(FILE *arq_arv, uint16_t rrn, uint32_t key_id, uint16_t key_offset, uint16_t *promo_r_child, uint32_t *promo_key_id, uint16_t *promo_key_offset)
+{
+	no_arvore_b_t pagina, nova_pagina;
+	int i, pos_insert, chamada;
+
+	if(rrn == 0)
+	{
+		*promo_key_id = key_id;
+		*promo_key_offset = key_offset;
+		*promo_r_child = 0;
+		return INSERTION_PROMOTION; 
+	}
+
+	fseek(arq_arv, RRN_to_POS(rrn), SEEK_SET);
+	Carregar_Pagina(arq_arv, &pagina);
+
+	i=0;
+	while(i< pagina.n && key_id > pagina.id[i])
+		++i;
+
+	if(i< pagina.n)
+		if(key_id == pagina.id[i])
+			return INSERTION_ERROR;
+
+	pos_insert = i;
+	chamada = Recursive_Insertion(arq_arv, pagina.filhas[pos_insert], key_id, key_offset, promo_r_child, promo_key_id, promo_key_offset);
+
+	switch(chamada)
+	{
+		case INSERTION_PROMOTION:
+			if(pagina.n < ORDEM_ARVORE -1)
+			{
+				//Abrir espaco para novo elemento na pagina
+				for(i=ORDEM_ARVORE -2; i> pos_insert; --i)
+				{
+					pagina.id[i] = pagina.id[i-1];
+					pagina.offset[i] = pagina.offset[i-1];
+					pagina.filhas[i+1] = pagina.filhas[i];
+				}
+
+				pagina.id[pos_insert] = *promo_key_id;
+				pagina.offset[pos_insert] = *promo_key_offset;
+				pagina.filhas[pos_insert +1] = *promo_r_child;
+				++(pagina.n);
+				fseek(arq_arv, RRN_to_POS(rrn), SEEK_SET);
+				Escrever_Pagina(arq_arv, pagina);
+				return INSERTION_NO_PROMOTION;
+			}
+			else
+			{
+				Insertion_Split(arq_arv, &pagina, pos_insert, promo_r_child, promo_key_id, promo_key_offset);
+				fseek(arq_arv, RRN_to_POS(rrn), SEEK_SET);
+				Escrever_Pagina(arq_arv, pagina);
+				return INSERTION_PROMOTION;			
+			}
+		break;
+		case INSERTION_NO_PROMOTION:
+			return INSERTION_NO_PROMOTION;
+		break;
+		case INSERTION_ERROR:
+			return INSERTION_ERROR;
+		break;
+	}
+}
+
+void Print_Arvore_B(char *arquivo)
+{
+	int fim, rrn, i;
+	uint16_t raiz;
+	FILE *arq_arv;
+	no_arvore_b_t pagina;
+
+	arq_arv = fopen(arquivo, "r");
+	fseek(arq_arv, 0, SEEK_END);
+	fim = ftell(arq_arv);
+	fseek(arq_arv, 2, SEEK_SET);
+	fread(&raiz, sizeof(uint16_t), 1, arq_arv);
+
+	while(ftell(arq_arv) != fim)
+	{
+		rrn = Carregar_Pagina(arq_arv, &pagina);
+		if(rrn == raiz)
+			printf("====\nRAIZ\n====\n");
+		printf("Pagina de RRN %d:\n", rrn);
+
+		for(i=0; i< pagina.n; ++i)
+			printf("\tChave de ID %d, offset 0x%04x\n", pagina.id[i], pagina.offset[i]);
+		printf("\tPaginas Filhas: ");
+		for(i=0; i< pagina.n +1; ++i)
+			printf("%d ", pagina.filhas[i]);
+		printf("\n\n");
+	}
+
+	fclose(arq_arv);
 }
 
 int main()
 {
 	char fileh[] = "./res/arvreb.btr\0";
 	FILE *arvre;
+	uint8_t flag;
+	int i;
 
-	arvre = fopen(fileh, "r+");
-	if(arvre == NULL)
-		arvre = Inicializar_ArvoreB(fileh);
+	srand(time(NULL));
+	flag = Inicializar_ArvoreB(fileh);
+	if(flag == INICIALIZA_ARVORE_ERRO)
+		return 1;
 
-	Inserir_No_ArvoreB(fileh, 24, 666);
+	for(i=0; i< 100; ++i)
+		Inserir_No_ArvoreB(fileh, rand() %100, rand() %0xffff);
 
-	fclose(arvre);
+	Print_Arvore_B(fileh);
 	return 0;
 }
+
--- include/arvore_b.h
+++ include/arvore_b.h
@@ -35,11 +35,18 @@
 
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define ORDEM_ARVORE 5
+
 #define INSERTION_ERROR 0
 #define INSERTION_NO_PROMOTION 1
 #define INSERTION_PROMOTION 2
+
+#define INICIALIZA_ARVORE_ERRO 0 
+#define INICIALIZA_ARVORE_SUCESSO 1 
+#define INICIALIZA_ARVORE_JAEXISTE 2 
 
 typedef struct no_arvore_b_t
 {
@@ -49,8 +56,15 @@ typedef struct no_arvore_b_t
 	uint16_t filhas[ORDEM_ARVORE]; //RRN das paginas filhas dessa pagina
 }no_arvore_b_t;
 
-uint16_t Criar_Pagina(FILE *arq_arv, no_arvore_b_t nova);
+uint16_t Carregar_Pagina(FILE *arq_arv, no_arvore_b_t *pagina); //Carrega uma pagina da arvore B contida em arq_arv, assumindo que o ponteiro de arquivo estah no inicio de um registro de pagina. Carrega a pagina na struct correspondente e retorna o RRN da pagina carregada
+uint16_t Criar_Pagina(FILE *arq_arv, no_arvore_b_t nova); //Cria nova pagina no arquivo, inserindo-a no primeiro espaco vazio da PED ou no final do arquivo, caso PED esteja vazia
+uint16_t Escrever_Pagina(FILE *arq_arv, no_arvore_b_t pagina); //Escreve uma pagina da arvore B no arquivo arq_arv, assumindo que o ponteiro de arquivo ja esteja na posicao correta
 uint16_t Inserir_No_ArvoreB(char *arquivo, uint32_t id, uint16_t offset); //Recebe o id e o offset de um registro, retorna o RRN em que o no foi inserido na arvore
-FILE *Inicializar_ArvoreB(char *arquivo); //Cria uma arvore B com cabecalho: PED = RRN 0 (pilha de espacos disponiveis vazia), Raiz = RRN 0 (Primeiro elemento possui RRN 1. O valor 0 indica que a arvore estah vazia
+uint8_t Inicializar_ArvoreB(char *arquivo); //Cria uma arvore B com cabecalho: PED = RRN 0 (pilha de espacos disponiveis vazia), Raiz = RRN 0 (Primeiro elemento possui RRN 1. O valor 0 indica que a arvore estah vazia
+void Insertion_Split(FILE *arq_arv, no_arvore_b_t *pagina, int pos_insert, uint16_t *promo_r_child, uint32_t *promo_key_id, uint16_t *promo_key_offset); //Funcao que trata a divisao de uma pagina quando a insercao de key_id provoca um overflow nela. O elemento promovido eh devolvido nas variaveis promo_key
+uint16_t POS_to_RRN(uint16_t POS);
+uint8_t Recursive_Insertion(FILE *arq_arv, uint16_t rrn, uint32_t key_id, uint16_t key_offset, uint16_t *promo_r_child, uint32_t *promo_key_id, uint16_t *promo_key_offset); //Funcao recursiva de insercao, conforme apresentada nos slides da materia
+uint16_t RRN_to_POS(uint16_t RRN);
+void Print_Arvore_B(char *arquivo); //Imprime todos os nos do arquivo (na sequencia do proprio arquivo)
 
 #endif
